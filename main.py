5c1c6c7e856f8b0336a79bb231be817a7ae5631f
medvedevads <147747911+medvedevads@users.noreply.github.com>
2024-03-22 15:09:17 +0300

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Создайте модуль приложения и настройте сервер и маршрутизацию.
 # Создайте класс User с полями id, name, email и password.
 # Создайте список users для хранения пользователей.
-# Создайте маршрут для отображения списка пользователей (метод GET).
+# Создайте маршрут для отображения списка пользователей (метод GET, POST, PUT, DELETE).
 # Реализуйте вывод списка пользователей через шаблонизатор Jinja.
 
 
@@ -40,6 +40,27 @@ async def root():
 async def get_users(request: Request):
     return templates.TemplateResponse("index.html", {"request": request,
                                                      "users": users})
+
+@app.post('/user/')
+async def creat_user(user: User):
+    users.append(user)
+    return user
+
+
+@app.put('/user/{id_}')
+async def update_user(id_: int, user: User):
+    for i in range(len(users)):
+        if users[i].id_ == id_:
+            users[i] = user
+    return {'id_': id_, 'user': user}
+
+
+@app.delete('/user/{id_}')
+async def delete_user(id_: int):
+    for us in users:
+        if us.id_ == id_:
+            users.remove(us)
+    return {'id_': id_}
 
 
 if __name__ == '__main__':
